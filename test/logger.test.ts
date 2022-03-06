1b789e795feb489090ee8ab8c36c8215de5b99a0
Eric <etavela@gmail.com>
2022-03-06 10:55:27 -0500

    Revised logging and test

--- test/logger.test.ts
+++ test/logger.test.ts
@@ -1,7 +1,39 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { logger } from '../src/logger'
 
+const TEST_MESSAGE = 'test message'
+const TIMESTAMP_REGEX =
+  /\d{4}-[01]\d-[0-3]\dT[0-2](?:\d:[0-5]){2}\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
+
 describe('logger', () => {
-  test('should provide a usable logger', () => {
-    logger.info('testing...')
+  let spy: any
+
+  beforeEach(() => {
+    spy = jest
+      .spyOn((console as any)._stdout, 'write')
+      .mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    spy.mockClear()
+  })
+
+  afterAll(() => {
+    spy.mockRestore()
+  })
+
+  test('should log stdout', () => {
+    logger.info(TEST_MESSAGE)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('should log expected fields', () => {
+    logger.info(TEST_MESSAGE)
+    const logged = JSON.parse(spy.mock.calls[0][0])
+    expect(logged.timestamp).toMatch(TIMESTAMP_REGEX)
+    expect(logged.level).toBe('info')
+    expect(logged.message).toBe(TEST_MESSAGE)
   })
 })
