b1e89e878a1d04fb4c08f40fdaf2207d40b1cba8
Eric <etavela@gmail.com>
2022-05-23 06:26:52 -0400

    Fixing Cassandra client initialization issue

--- src/cassandra/client.ts
+++ src/cassandra/client.ts
@@ -3,21 +3,26 @@ import { ContainerModule, interfaces } from 'inversify'
 
 import { getEnvironmentVariable } from '../utils'
 
-const client = new Client({
-  contactPoints: [getEnvironmentVariable('CASSANDRA_DOMAIN')],
-  credentials: {
-    username: getEnvironmentVariable('CASSANDRA_USERNAME'),
-    password: getEnvironmentVariable('CASSANDRA_PASSWORD')
-  },
-  localDataCenter: getEnvironmentVariable('CASSANDRA_DATACENTER'),
-  keyspace: getEnvironmentVariable('CASSANDRA_KEYSPACE'),
-  protocolOptions: {
-    port: Number.parseInt(getEnvironmentVariable('CASSANDRA_PORT', '9042'), 10)
-  }
-})
-
-client.connect()
+function makeClient() {
+  const client = new Client({
+    contactPoints: [getEnvironmentVariable('CASSANDRA_DOMAIN')],
+    credentials: {
+      username: getEnvironmentVariable('CASSANDRA_USERNAME'),
+      password: getEnvironmentVariable('CASSANDRA_PASSWORD')
+    },
+    localDataCenter: getEnvironmentVariable('CASSANDRA_DATACENTER'),
+    keyspace: getEnvironmentVariable('CASSANDRA_KEYSPACE'),
+    protocolOptions: {
+      port: Number.parseInt(
+        getEnvironmentVariable('CASSANDRA_PORT', '9042'),
+        10
+      )
+    }
+  })
+  client.connect()
+  return client
+}
 
 export const cassandraModule = new ContainerModule((bind: interfaces.Bind) => {
-  bind(Client).toConstantValue(client)
+  bind(Client).toConstantValue(makeClient())
 })
